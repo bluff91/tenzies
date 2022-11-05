166848d028041da4504fc936517f22168dd85a42
bluff <bluffyx91@gmail.com>
2022-11-05 22:17:03 +0200

    created layout with styles, Die component and generation of random dice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 
+import { useState } from 'react';
 import './App.css';
+import Die from './components/Die';
+import uniqid from 'uniqid';
+
 
 function App() {
+  const [dice, setDice] = useState(allNewDice)
+
+  function allNewDice() {
+    const dieArr = []
+    for (let i=0; i<10; i++) {
+      dieArr.push({value: Math.ceil(Math.random() * 6), 
+                  isHeld: false,
+                  id: uniqid()
+                  })
+    }
+    return dieArr;
+  }
+
+  const diceElements = dice.map(item => <Die value={item.value} isHeld={item.isHeld}/>)
+
   return (
-    <div className="App">
+    <main className="App">
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button
+        className='roll-dice'
+        onClick={() => setDice(allNewDice)}
+      >Roll</button>
       
-    </div>
+      
+    </main>
   );
 }
 
